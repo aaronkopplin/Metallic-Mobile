eccba2bd57628b9787003080877eb68c1f2190e4
aaron <kopplina@mail.gvsu.edu>
2021-01-10 13:07:08 -0500

    creating metallic mobile

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Button, Text, View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import HomeScreen from "./screens/home";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const App = () => {
+    return <HomeScreen />;
+};
+
+export default App;
---
+++ screens/feed.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { Button, Text, View } from "react-native";
+
+function FeedScreen() {
+    return (
+        <View
+            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+            <Text>Feed screen</Text>
+        </View>
+    );
+}
+
+export default FeedScreen;
